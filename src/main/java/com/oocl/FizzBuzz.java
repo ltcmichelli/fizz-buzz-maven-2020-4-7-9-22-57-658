a239167255a945e380469b04624cfb3e50f74901
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-08 16:06:40 +0800

    Refactor the code

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -2,17 +2,15 @@ package com.oocl;
 
 public class FizzBuzz {
     public String say(int input) {
-        if (input % 3 == 0 && input % 5 == 0) {
-            return "FizzBuzz";
-        }
+        String result = "";
 
         if (input % 3 == 0) {
-            return "Fizz";
+            result += "Fizz";
         }
         if (input % 5 == 0) {
-            return "Buzz";
+            result += "Buzz";
         }
 
-        return String.valueOf(input);
+        return result.equals("") ? String.valueOf(input) : result;
     }
 }
